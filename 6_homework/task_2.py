05a9fba56381554c038881c13cd5c54e58800b69
Nata Kregel <natik.kregel@gmai.com>
2022-11-12 19:51:59 +0300

    Семинар 6.Задача 2

--- 6_homework/task_2.py
+++ 6_homework/task_2.py
@@ -2,25 +2,12 @@
 #[12,'sadf',5643] ---> ['sadf'] ,[12,5643]
 
 
-#n = [12,'sadf',5643]
-#print(*list(filter(lambda x: x.isdigit(),int(n))))
-'''
-s = "12,sadf,5643"
-import re
-s1 = re.sub("[0-9]", "", s)
-print(s1)
-s2 = "".join(c for c in s if  c.isdecimal())
-print(s2)
-'''
+list0 = [12,'sadf',5643]
+list_num = list(filter(lambda x: type(x) is int, list0))
+print(list_num)
 
-num = ['12','sadf','5643']
-f = filter(lambda x: x.replace('.', '').isdigit(), num)
-f = list(f) 
-print(f)
+list0 = [12,'sadf',5643]
+list_str = list(filter(lambda x: type(x) is str, list0))
+print(list_str)
 
-s = [12,'sadf',5643]
-ss = list(filter(None, s))
-print(ss)
-
-                   
 
