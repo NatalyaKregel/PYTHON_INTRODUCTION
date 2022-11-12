e737500c6d66465eb04ed85f572ecb6c7ad594ce
Nata Kregel <natik.kregel@gmai.com>
2022-11-13 01:11:13 +0300

    Семинар 7.Калькулятор

--- 7_seminar/log.py
+++ 7_seminar/log.py
@@ -1,5 +1,5 @@
 def loger(primer, result):
-    with open('file.txt', 'a') as data: # a - создаст файл, если его не будет и запишет в него
+    with open('file7.txt', 'a') as data: # a - создаст файл, если его не будет и запишет в него
         
         rasparse =''.join(map(str,primer))      # соединить список в строку
         rasparse = rasparse + ' = '
